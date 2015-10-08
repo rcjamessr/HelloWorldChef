adbaba471200e36f52989280087a2ac6e9e51e53
rcjamessr <rcjamessr@gmail.com>
2015-10-08 15:38:33 -0400

    from lt prtv

--- src/HelloWorldChef.java
+++ src/HelloWorldChef.java
@@ -4,7 +4,8 @@ public class HelloWorldChef {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World of Chef");
-		System.out.println("This is version 2");
+		
+		System.out.println("This is version 2.1");
 	}
 
 }
