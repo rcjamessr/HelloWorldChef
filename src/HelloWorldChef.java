02ee16f80d747d76c4e53aabd328b87e57bf6b10
rcjamessr <rcjamessr@gmail.com>
2015-10-08 13:40:36 -0400

    version 1.1

--- src/HelloWorldChef.java
+++ src/HelloWorldChef.java
@@ -4,6 +4,7 @@ public class HelloWorldChef {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World of Chef");
+		System.out.println("This is version 2");
 	}
 
 }
