9186af99f5030249930244ceaa4418f37a390166
rcjamessr <rcjamessr@gmail.com>
2015-10-09 17:43:33 -0400

    conf file

--- src/HelloWorldChef.java
+++ src/HelloWorldChef.java
@@ -1,3 +1,7 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class HelloWorldChef {
 
@@ -5,6 +9,33 @@ public class HelloWorldChef {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World of Chef");
 		System.out.println("This is version 2.1");
+		String current = new File( "." ).getAbsolutePath();
+		current = current.subSequence(0,current.length()-1).toString();
+		String path = current + "src/helloworld.conf";
+		String[] attr = readFile(path).split(":");
+		System.out.println(attr[1]);
+		
+		
 	}
+	public static final String readFile(String filePath) {
+        File f = new File(filePath);
+        if(!f.exists()) {
+            System.err.println("File " + f.getName() + " not found");
+            return null;
+        }
 
+        StringBuilder buffer = new StringBuilder();
+        byte[] bytes = new byte[1024];
+        int readBytesCount = 0;
+        try {
+            InputStream in = new BufferedInputStream(new FileInputStream(f));
+            while( (readBytesCount = in.read(bytes)) != -1) {
+                buffer.append(new String(bytes, 0, readBytesCount));
+            }
+        } catch(Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+        return buffer.toString();
+    }
 }
